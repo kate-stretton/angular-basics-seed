17b45e63a60624db63e806243b83af8a32fae3b2
= <=>
2023-05-19 21:16:18 +1200

    render donut info using ngonInit lifecycle hook

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+  <div>
+    <div>{{donut.name }}</div>
+    <div>{{donut.price }}</div>
+  </div>
   `,
   styles: [
   ]
 })
-export class DonutListComponent {
+export class DonutListComponent implements OnInit {
+  donut!: any; 
+  donuts!: any[];
 
+  constructor(){}
+
+  ngOnInit(): void {
+    this.donuts = [
+      {id: '1f1e2t',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 5,
+      description: 'For the pure chocoholic'
+      },
+      {id: '13g98d',
+      name: 'Lemon',
+      icon: 'lemon',
+      price: 5,
+      description: 'Lemonade'
+      },
+      {id: '3kd98s',
+      name: 'Raspberry',
+      icon: 'raspberry',
+      price: 5,
+      description: 'Raspberry donut'
+      } 
+    ];
+
+    this.donut = this.donuts[0];
+  }
 }
