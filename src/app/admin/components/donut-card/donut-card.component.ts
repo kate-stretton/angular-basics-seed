3bf64e18ab6e01f74c8b03e606336e7659b7f68f
= <=>
2023-05-20 10:15:31 +1200

    add css

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -19,10 +19,32 @@ import { Donut } from '../../models/donut.model';
     </div>
   `,
   styles: [
+    `
+    .donut-card {
+      display:flex;
+      align-items: center; 
+      background: #f7f7f7;
+      border-radius: 5px;
+      maring-bottom: 5px;
+      padding: 5px 15px;
+      transition: transform 0.2s ease-in-out;
+      &:hover {
+        transform: tanslateY(-3px);
+      }
+      &-name {
+        font-size: 14px;
+        color: #c14583
+      }
+      &-icon {
+        width: 50px;
+        margin-right: 10px
+      }
+    }
+    `
   ]
 })
 export class DonutCardComponent {
-  @Input() donut!: Donut
+  @Input() donut!: Donut;
   
   constructor() {}
 }
