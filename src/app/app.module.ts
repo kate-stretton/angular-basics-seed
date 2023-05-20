391e1d5f4472d662f862d4c24fcf844da7da5a0e
= <=>
2023-05-21 09:13:52 +1200

    create a form container and a dumb component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// containers
 import { AppComponent } from './app.component';
 import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
+
+//components
 import { DonutCardComponent } from './admin/components/donut-card/donut-card.component';
+import { DonutFormComponent } from './admin/components/donut-form/donut-form.component';
 
 @NgModule({
-  declarations: [AppComponent, DonutListComponent, DonutCardComponent],
+  declarations: [AppComponent, DonutListComponent, DonutCardComponent, DonutSingleComponent, DonutFormComponent],
   imports: [BrowserModule],
-  exports: [DonutListComponent],
+  exports: [DonutListComponent, DonutSingleComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
