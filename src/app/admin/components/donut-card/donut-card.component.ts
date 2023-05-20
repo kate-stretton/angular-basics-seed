978a5cd477cadabab933b43d68e1f52fcb86d676
= <=>
2023-05-20 22:11:34 +1200

    use a pipe to format currency

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -20,7 +20,7 @@ import { Donut } from '../../models/donut.model';
         {{ donut.name }}
       </p>
       <p class="donut-card-price">
-        {{ donut.price }}
+        {{ donut.price / 100 | currency: 'NZD' : 'symbol' }}
       </p>
     </div>
   </div>
