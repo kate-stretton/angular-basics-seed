b194e29262ee2cf827acf23a041d9a6afe4cb720
= <=>
2023-05-21 15:20:18 +1200

    lazy-loading feature modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-// containers
 import { AppComponent } from './app.component';
-import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
-import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
-
-//components
-import { DonutCardComponent } from './admin/components/donut-card/donut-card.component';
-import { DonutFormComponent } from './admin/components/donut-form/donut-form.component';
 
 export const routes: Routes = [
-  { 
-    path: 'admin', 
-    children: [
-      { path: 'donuts', component: DonutListComponent },
-      { path: 'donut', component: DonutSingleComponent },
-      { path: '', pathMatch: 'full', redirectTo: 'donuts'}
-    ]
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((x) => x.AdminModule),
   },
   {
     path: '',
@@ -30,13 +19,12 @@ export const routes: Routes = [
   {
     path: '**',
     redirectTo: 'admin',
-  }
+  },
 ];
 
 @NgModule({
-  declarations: [AppComponent, DonutListComponent, DonutCardComponent, DonutSingleComponent, DonutFormComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(routes)],
-  exports: [DonutListComponent, DonutSingleComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
