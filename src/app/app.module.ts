492067b24359951143acef105620fc65a48cd59c
= <=>
2023-05-21 15:05:31 +1200

    created nested child routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,13 @@ import { DonutCardComponent } from './admin/components/donut-card/donut-card.com
 import { DonutFormComponent } from './admin/components/donut-form/donut-form.component';
 
 export const routes: Routes = [
-  { path: '', component: DonutListComponent },
-  { path: 'donut', component: DonutSingleComponent}
+  { 
+    path: 'admin', 
+    children: [
+      { path: 'donuts', component: DonutListComponent },
+      { path: 'donut', component: DonutSingleComponent } 
+    ]
+  }
 ];
 
 @NgModule({
